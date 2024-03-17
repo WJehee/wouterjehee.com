0bfa4e291590a53ab84d1bfcfbafeb254797cb01
Wouter Jehee <wouter@wouterjehee.com>
2024-03-17 10:09:50 +0100

    update catalogue, add flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+    description = "Personal website";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }:
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+        devShells.${system}.default = with pkgs; mkShell {
+            buildInputs = [ zola ];
+        };
+    };
+}
